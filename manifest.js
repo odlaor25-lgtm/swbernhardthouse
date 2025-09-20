5760f4a3338116b95f88239700c26285b810ef0f
odlaor25-lgtm <odlaor25@gmail.com>
2025-09-20 18:47:16 +0700

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,15 @@
+﻿{
+  "name": "SWB Matrix App",
+  "short_name": "SWB",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#0a0a0a",
+  "theme_color": "#00ff64",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
---
+++ service-worker.js
@@ -0,0 +1,19 @@
+﻿self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("swb-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
